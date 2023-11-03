aa62c1336a30120f4a21a17f4f8b06c4cc280c9a
DavidGmc18 <davidgames.youtub@gmail.com>
2023-11-03 17:25:03 +0100

    Initial commit

---
+++ Example/Fabric/src/main/java/example/ExampleMod.java
@@ -0,0 +1,12 @@
+package example;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+    public static final String MOD_ID = "examplemod";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ Example/Forge/src/main/java/example/ExampleMod.java
@@ -0,0 +1,14 @@
+package example;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(ExampleMod.MOD_ID)
+public class ExampleMod {
+    public static final String MOD_ID = "examplemod";
+
+    public ExampleMod() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+    }
+}
